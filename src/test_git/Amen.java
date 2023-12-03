980cb22e7e532f1169fcfecf48b85d5be81f1e63
imkh817 <rjs7180@naver.com>
2023-12-03 21:35:08 +0900

    kunhee commit2

--- src/test_git/Amen.java
+++ src/test_git/Amen.java
@@ -4,6 +4,9 @@ public class Amen {
 
 	public static void main(String[] args) {
 		System.out.println("아멘!!!!!@@!!");
+		System.out.println("아멘!!!!!@@!!");
+		System.out.println("아멘!!!!!@@!!");
+		
 
 	}
 
